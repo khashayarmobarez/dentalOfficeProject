7bb52bc0c94c283674526eea0540f8c52b2c45ba
khashayar <khashayarmobarezbusiness@gmail.com>
2023-06-10 03:10:36 +0330

    added a loader

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './home elements/Home.module.css'
 
-import Slider from './home elements/Slider';
+import SliderLoader from './home elements/Slider';
 import ClinicPics from './home elements/ClinicPics';
 import Biography from './home elements/Biography';
 import Certifications from './home elements/Certifications';
@@ -12,7 +12,7 @@ import Insta from './home elements/Insta';
 const Home = () => {
     return (
         <div className={styles.container}>
-           <Slider />
+           <SliderLoader />
            <ClinicPics />
            <Biography />
            <Certifications/>
--- src/Components/home elements/Slider.js
+++ src/Components/home elements/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
@@ -9,8 +9,33 @@ import pic1 from '../../media/pictures/Clinic pics/clinic pic 1 cropped.jpg';
 import pic2 from '../../media/pictures/Clinic pics/clinic pic 2.jpg';
 import pic3 from '../../media/pictures/Clinic pics/clinic pic 3.jpg'
 
+const SliderLoader = () => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      // Simulate loading time
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }, []);
+  
+    if (isLoading) {
+      return (
+        <div className={styles.loaderContainer}>
+             <div className={styles.loader}>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+            </div>
+        </div>
+      );
+    }
+  
+    return <Slider />;
+  };
 
-const slider = () => {
+const Slider = () => {
     return (
         <div className={styles.sliderContainer}>
 
@@ -45,4 +70,4 @@ const slider = () => {
     );
 };
 
-export default slider;
+export default SliderLoader;
