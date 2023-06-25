1238673a5a6f6dd7a4f3dc336861e574182e5fe5
khashayar <khashayarmobarezbusiness@gmail.com>
2023-06-25 19:49:50 +0330

    improvments

--- src/Components/education/Video.js
+++ src/Components/education/Video.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './Video.module.css'
-import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
+
+import sharebtn from '../../media/svg/shareIcon.svg'
 
 const Video = (props) => {
 
     const {video_name,thumbnail,duration,video_player} = props.videoData;
 
+    const handleShare = () => {
+        if (navigator.share) {
+          navigator.share({
+            title: 'dr.babak zandi website',
+            text: {video_name},
+            url: {video_player}
+          })
+            .then(() => console.log('Link shared successfully.'))
+            .catch((error) => console.error('Error sharing link:', error));
+        } else {
+          console.log('Web Share API is not supported on this browser.');
+        }
+      };
 
     return (
         <div className={styles.card}>
@@ -22,9 +36,9 @@ const Video = (props) => {
                         <a href={video_player} target='_blank' rel='noreferrer' >
                             <button className={styles.btn}>پخش</button>
                         </a>
-                        <Link to='/soon'>
-                            <button className={styles.btn}>توضیحات</button>
-                        </Link>
+
+                            <button onClick={handleShare} className={styles.btn}><img src={sharebtn} alt='sharebtn' /></button>
+
                     </div>
             </div>
         </div>
--- src/Components/education/Videos.js
+++ src/Components/education/Videos.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './Videos.module.css'
 
 import Video from './Video';
@@ -6,12 +6,40 @@ import Video from './Video';
 import { videoContext } from './Contexts/VideoContextProvider'
 
 
+const VideosLoader = () => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      // Simulate loading time
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2500);
+    }, []);
+  
+    if (isLoading) {
+      return (
+        <div className={styles.loaderContainer}>
+             <div className={styles.threeBody}>
+                <div className={styles.threeBodyDot}></div>
+                <div className={styles.threeBodyDot}></div>
+                <div className={styles.threeBodyDot}></div>
+            </div>
+        </div>
+      );
+    }
+  
+    return <Videos />;
+  };
+
 const Videos = () => {
 
     const videos = useContext(videoContext)
 
     return (
         <div className={styles.Container}>
+          <div className={styles.titleContainer}>
+            <h1 >ویدیوهای اموزشی اندودونتیکس (endodontics)</h1>
+          </div>
             <div className={styles.VideosContainer} >
                 {
                 videos.map(video => <Video 
@@ -25,4 +53,4 @@ const Videos = () => {
     );
 };
 
-export default Videos;
+export default VideosLoader;
